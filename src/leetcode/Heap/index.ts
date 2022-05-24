3b269060bd44f84913cbc9701cba2166fbceffc6
powerfulyang <i@powerfulyang.com>
2022-05-24 14:59:01 +0800

    feat: Heap

---
+++ src/leetcode/Heap/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Heap is array-based data structure.
+ * It is a complete binary tree.
+ * The root node is the largest element.
+ */
+export class Heap {}
+
+// Online Algorithm (using heap)
+// 针对一组流数据，没有固定长度，可以随时根据需求 scalable
+
+// Offline Algorithm (using sorting)
+// 针对一组固定长度的数据，每次 scale 后需要重新计算
