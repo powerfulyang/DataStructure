af6022d551b604e7e61c3fac215d49c6001a445f
powerfulyang <i@powerfulyang.com>
2022-07-11 22:04:09 +0800

    feat: HashTable

---
+++ src/leetcode/HashTable/TwoSum.ts
@@ -0,0 +1,11 @@
+export const TwoSum = (nums: number[], target: number) => {
+  const helperMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const rIndex = helperMap.get(target - nums[i]);
+    if (rIndex !== undefined) {
+      return [rIndex, i];
+    }
+    helperMap.set(nums[i], i);
+  }
+  return [];
+};
